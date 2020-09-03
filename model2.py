91df0fb1e3265033c48b1b10c06d8628512a99b3
yxqAIxp <xiuqiangye@163.com>
2020-09-03 20:04:08 +0800

    update resNest50 model

--- model2.py
+++ model2.py
@@ -1142,5 +1142,29 @@ class HighResolutionNet(nn.Module):
         # print(out.size(), axn_input.size())
         return out, axn_input
 
+# resNest50
+from resnest.torch import resnest50
+class MyResNest50(nn.Module):
+    
+    def __init__(self, nums_class=136):
+        super(MyResNest50, self).__init__()
 
+        self.resnest = resnest50(pretrained=True)
+        self.resnest_backbone1 = nn.Sequential(*list(self.resnest.children())[:-6])
+        self.resnest_backbone_end = nn.Sequential(*list(self.resnest.children())[-6:-2])
+        
+        self.in_features = 2048 * 4 * 4
+        self.fc = nn.Linear(in_features=self.in_features, out_features=nums_class)
+
+    def forward(self, x):
+        # (x has shape (batch_size, 3, h, w))
+
+        # pass x through (parts of) the pretrained ResNet:
+        auxnet = self.resnest_backbone1(x)
+        #print(auxnet.size())
+        out = self.resnest_backbone_end(auxnet)
+        #print(out.size())
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out, auxnet
 
